c746a4a3335d0b5d2003a3d4502b66b91262ca75
06kellyjac <dev@j-k.io>
2021-03-15 22:05:59 +0000

    firefox-addons.decentraleyes: init at 2.0.15

--- pkgs/firefox-addons/default.nix
+++ pkgs/firefox-addons/default.nix
@@ -4,4 +4,5 @@
   _1password-x = callPackage ./1password-x { };
   auto-tab-discard = callPackage ./auto-tab-discard { };
   clearurls = callPackage ./clearurls { };
+  decentraleyes = callPackage ./decentraleyes { };
 }
