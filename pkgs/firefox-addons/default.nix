aff238116cfef873e229639ff4d69d6403120ae1
06kellyjac <dev@j-k.io>
2021-03-15 22:06:23 +0000

    firefox-addons.disable-webrtc: init at 1.0.23

--- pkgs/firefox-addons/default.nix
+++ pkgs/firefox-addons/default.nix
@@ -5,4 +5,5 @@
   auto-tab-discard = callPackage ./auto-tab-discard { };
   clearurls = callPackage ./clearurls { };
   decentraleyes = callPackage ./decentraleyes { };
+  disable-webrtc = callPackage ./disable-webrtc { };
 }
