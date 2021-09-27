29b92f30e44222d82e6c84cea8b867a89a9d021f
06kellyjac <dev@j-k.io>
2021-09-27 16:56:16 +0100

    crane: drop

--- default.nix
+++ default.nix
@@ -25,7 +25,6 @@ rec {
   firefox-addons = recurseIntoAttrs (callPackage ./pkgs/firefox-addons { });
 
   bane = callPackage ./pkgs/bane { };
-  crane = callPackage ./pkgs/crane { };
   comma = callPackage ./pkgs/comma { };
   conform = callPackage ./pkgs/conform { };
   container-diff = callPackage ./pkgs/container-diff { };
